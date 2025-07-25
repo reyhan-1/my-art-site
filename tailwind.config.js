6a99021df1359a376982d2727d09604c742b2855
Reyhan Uyanik <ruyanik@axiomcloud.ai>
2025-07-25 22:56:45 +0300

    added mjs extention

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mjs}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx,mjs}",
+    "./app/**/*.{js,ts,jsx,tsx,mjs}",
   ],
   theme: {
     extend: {
