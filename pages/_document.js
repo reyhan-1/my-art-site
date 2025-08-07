0b1a7fd4d506d9854f08579c84a63c725b0a343f
Reyhan Uyanik <reyhanuyanik@icloud.com>
2025-08-08 01:07:44 +0300

    viewport

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document(props) {
-  // Get the current route path from Next.js data
   const currentPath = props.__NEXT_DATA__.page || '';
-
-  // Check if this is the homepage (adjust if you want other pages)
   const isHomePage = currentPath === '/';
 
   return (
@@ -13,6 +10,9 @@ export default function Document(props) {
       style={{ backgroundColor: isHomePage ? 'black' : 'white' }}
     >
       <Head>
+        {/* ✅ Add viewport meta tag */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         {/* Font preconnects */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
